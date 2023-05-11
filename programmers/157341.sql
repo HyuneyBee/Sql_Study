e6c23f036df5c100c5cd00093a376d7d805d945e
HyuneyBee <gsh2448@gmail.com>
2023-05-11 10:46:48 +0900

    157341 해결

---
+++ programmers/157341.sql
@@ -0,0 +1,12 @@
+SELECT
+    distinct(H.CAR_ID)
+FROM
+    CAR_RENTAL_COMPANY_CAR as C
+        JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY as H ON
+            C.CAR_ID = H.CAR_ID
+WHERE
+            C.CAR_TYPE = '세단'
+        AND
+        MONTH(H.START_DATE) = 10
+ORDER BY
+    H.CAR_ID DESC
